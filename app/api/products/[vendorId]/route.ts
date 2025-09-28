5231d029281713b020be1c32fc77d6e3a09acc38
Amitoj <amitoj.mehta@gmail.com>
2025-09-28 19:17:38 +0530

    admin pages

--- app/api/products/[vendorId]/route.ts
+++ app/api/products/[vendorId]/route.ts
@@ -1,31 +1,31 @@
 import { prisma } from "@/lib/db";
 import { getVendor } from "@/lib/helpers/auth";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
   context: { params: Promise<{ vendorId: string }> }
 ) {
   const { success, message, vendor } = await getVendor();
-  if (!success) return { success, message };
+  if (!success) return NextResponse.json({ success, message });
 
   try {
     const vendorId = (await context.params).vendorId;
-    if (!vendorId) return { success: false, message: "Vendor ID not found" };
+    if (!vendorId) return NextResponse.json({ success: false, message: "Vendor ID not found" });
 
     const vendorProfile = await prisma.vendorProfile.findUnique({
       where: { userId: vendorId },
     });
     if (!vendorProfile)
-      return { success: false, message: "Vendor profile not found" };
+      return NextResponse.json({ success: false, message: "Vendor profile not found" });
 
     const products = await prisma.product.findMany({
       where: { vendorId: vendorProfile.id },
     });
-    
-    return { success: true, products };
+
+    return NextResponse.json({ success: true, products });
   } catch (error) {
     console.error(error);
-    return { success: false, message: "Internal server error" };
+    return NextResponse.json({ success: false, message: "Internal server error" });
   }
 }
